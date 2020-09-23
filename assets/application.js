2c52930538870a0e6acdff62fd7aaa64c3f55542
Matt Roscoe <matthias@ignitehospitality.com>
2020-09-23 10:23:47 +0100

    home, product, cart, page, blog article templates done

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,27 @@
-// Put your application javascript here
+(function() {
+
+    function init() {
+        tabs();
+    }
+
+    /**
+     * Tabs section
+     */
+    
+    function tabs() {
+
+        jQuery('.js-tab').on('mouseover', function(e) {
+            const index = $(this).data('index');
+            $('.js-tab').removeClass('active');
+            $(this).addClass('active');
+    
+            $('.js-tab-content').removeClass('active');
+            $(`.js-tab-content[data-index="${index}"]`).addClass('active');
+        })
+
+    }
+
+
+    init();
+
+})();
